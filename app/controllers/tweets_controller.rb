8c4f227d938746be2ac1534d54e7ed448d69472e
hiroki000024 <king0000240000@gmail.com>
2019-12-28 00:21:31 +0900

    comments

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -24,7 +24,7 @@ class TweetsController < ApplicationController
   end
   def show
     @tweet = Tweet.find(params[:id])
-    @comment = @tweet.comments.includes(:user)
+    @comments = @tweet.comments.includes(:user)
   end
 
   private 
